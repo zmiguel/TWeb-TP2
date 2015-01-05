5ad7a95b51dd3901bd512a7472e486bec8857d6a
ZMiguel Alves <zmiguel.alves@gmail.com>
2015-01-05 23:06:06 +0000

    versao final v1

--- main.js
+++ main.js
@@ -9,12 +9,6 @@ $(document).ready(function(){
         $('#box').load('html/sobre.html');
     } else if(page == "#pessoal"){
         $('#box').load('html/pessoal.html');
-    } else if(page == "#presidente"){
-        $('#box').load('html/presidente.html');
-    } else if(page == "#tesoureiro"){
-        $('#box').load('html/tesoureiro.html');
-    } else if(page == "#vogais"){
-        $('#box').load('html/vogais.html');
     } else if(page == "#servicos"){
         $('#box').load('html/servicos.html');
     } else if(page == "#info"){
@@ -45,6 +39,38 @@ $(document).ready(function(){
         $('#box').load('html/azulejos.html');
     } else if(page == "#contactos"){
         $('#box').load('html/contactos.html');
+    } else if(page == "#1"){
+        $('#box').load('html/1.html');
+    } else if(page == "#2"){
+        $('#box').load('html/2.html');
+    } else if(page == "#3"){
+        $('#box').load('html/3.html');
+    } else if(page == "#4"){
+        $('#box').load('html/4.html');
+    } else if(page == "#5"){
+        $('#box').load('html/5.html');
+    } else if(page == "#6"){
+        $('#box').load('html/6.html');
+    } else if(page == "#7"){
+        $('#box').load('html/7.html');
+    } else if(page == "#8"){
+        $('#box').load('html/8.html');
+    } else if(page == "#9"){
+        $('#box').load('html/9.html');
+    } else if(page == "#10"){
+        $('#box').load('html/10.html');
+    } else if(page == "#11"){
+        $('#box').load('html/11.html');
+    } else if(page == "#12"){
+        $('#box').load('html/12.html');
+    } else if(page == "#13"){
+        $('#box').load('html/13.html');
+    } else if(page == "#14"){
+        $('#box').load('html/14.html');
+    } else if(page == "#15"){
+        $('#box').load('html/15.html');
+    } else if(page == "#16"){
+        $('#box').load('html/16.html');
     } else {
         $('#box').load('html/404.html');
     }
@@ -66,17 +92,11 @@ $(window).resize(function(){
 $(window).on('hashchange',function(){ 
     var page = window.location.hash;
     if(page == ""){
-        $('#box').load('html/home.html');
+       $('#box').load('html/home.html');
     } else if(page == "#sobre"){
         $('#box').load('html/sobre.html');
     } else if(page == "#pessoal"){
         $('#box').load('html/pessoal.html');
-    } else if(page == "#presidente"){
-        $('#box').load('html/presidente.html');
-    } else if(page == "#tesoureiro"){
-        $('#box').load('html/tesoureiro.html');
-    } else if(page == "#vogais"){
-        $('#box').load('html/vogais.html');
     } else if(page == "#servicos"){
         $('#box').load('html/servicos.html');
     } else if(page == "#info"){
@@ -107,6 +127,38 @@ $(window).on('hashchange',function(){
         $('#box').load('html/azulejos.html');
     } else if(page == "#contactos"){
         $('#box').load('html/contactos.html');
+    } else if(page == "#1"){
+        $('#box').load('html/1.html');
+    } else if(page == "#2"){
+        $('#box').load('html/2.html');
+    } else if(page == "#3"){
+        $('#box').load('html/3.html');
+    } else if(page == "#4"){
+        $('#box').load('html/4.html');
+    } else if(page == "#5"){
+        $('#box').load('html/5.html');
+    } else if(page == "#6"){
+        $('#box').load('html/6.html');
+    } else if(page == "#7"){
+        $('#box').load('html/7.html');
+    } else if(page == "#8"){
+        $('#box').load('html/8.html');
+    } else if(page == "#9"){
+        $('#box').load('html/9.html');
+    } else if(page == "#10"){
+        $('#box').load('html/10.html');
+    } else if(page == "#11"){
+        $('#box').load('html/11.html');
+    } else if(page == "#12"){
+        $('#box').load('html/12.html');
+    } else if(page == "#13"){
+        $('#box').load('html/13.html');
+    } else if(page == "#14"){
+        $('#box').load('html/14.html');
+    } else if(page == "#15"){
+        $('#box').load('html/15.html');
+    } else if(page == "#16"){
+        $('#box').load('html/16.html');
     } else {
         $('#box').load('html/404.html');
     }
